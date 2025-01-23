30dc5f500bb738e305eb9746f7e39c11d3f9dcbb
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-01-22 22:47:50 -0800

    remove regular expression

--- script.js
+++ script.js
@@ -7,12 +7,21 @@ document.getElementById('click').onclick = function()
     for(let i = 0; i < passwordNeeds.length; i++){
         if(!fName.includes(passwordNeeds[i])){
             // Password does not meet requirement
-        }else if(!/[A-Z]/.test(fName) || !/[a-z]/.test(fName)){
-            // Password does not have a mix of capitals and lower case
         }else if (passwordNeeds.length === passwordMinimunSize){
             // Password too short
         }else{
             // Password is good
         }
     }
+
+    // else if(!/[A-Z]/.test(fName) || !/[a-z]/.test(fName)){
+    //     // Password does not have a mix of capitals and lower case
+}
+
+function hasUpperAndLowerCase(){
+
+}
+
+function hasDigits(){
+
 }
