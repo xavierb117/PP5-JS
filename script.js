bbb8d56bd30a4af1e616ac03fd4f67e7e589faf1
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-01-22 22:16:27 -0800

    js logic, still needs to be tuned

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 document.getElementById('click').onclick = function()
 {
     let fName = document.getElementById('password').value;
-    let passwordNeeds[] = ["!", "@","#","$","%","^","&","*",]; 
+    let passwordNeeds = ["!", "@","#","$","%","^","&","*",]; 
+    let passwordMinimunSize;
 
-    for(let i = 0; i < passwordNeeds.length-1; i++){
-        
+    for(let i = 0; i < passwordNeeds.length; i++){
+        if(!fName.includes(passwordNeeds[i])){
+            // Password does not meet requirement
+        }else if(!/[A-Z]/.test(fName) || !/[a-z]/.test(fName)){
+            // Password does not have a mix of capitals and lower case
+        }else if (passwordNeeds.length === passwordMinimunSize){
+            // Password too short
+        }else{
+            // Password is good
+        }
     }
 }
